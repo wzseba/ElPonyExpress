38c093dfd39a66beaacf99ba43f09719f96936d0
wzseba <wzseba@gmail.com>
2024-10-07 23:19:58 -0300

    add: logica en desarrollo

--- src/logistica/Jinete.java
+++ src/logistica/Jinete.java
@@ -2,4 +2,33 @@ package logistica;
 
 public class Jinete {
 
+	private int cantCaballo;
+
+	public Jinete() {
+		this.cantCaballo = 0;
+	}
+
+	public int getCaballo() {
+		return cantCaballo;
+	}
+
+	public void cambiarCaballo(int caballo) {
+		cantCaballo = caballo;
+	}
+
+	public int caballos(int[] distancias) {
+
+		int distEstaciones = 0;
+
+		for (int i = 0; i < distancias.length; i++) {
+
+			distEstaciones = distancias[i] + distancias[i + 1];
+			if (distEstaciones <= 100) {
+				cambiarCaballo(getCaballo() + 1);
+			}
+		}
+
+		return this.cantCaballo;
+	}
+
 }
