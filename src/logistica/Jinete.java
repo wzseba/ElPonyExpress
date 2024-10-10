49d116daac864bd4d8198a35db8fa1b3b2288abe
wzseba <wzseba@gmail.com>
2024-10-09 22:15:43 -0300

    add: logica y testing

--- src/logistica/Jinete.java
+++ src/logistica/Jinete.java
@@ -2,33 +2,40 @@ package logistica;
 
 public class Jinete {
 
-	private int cantCaballo;
-
 	public Jinete() {
-		this.cantCaballo = 0;
-	}
-
-	public int getCaballo() {
-		return cantCaballo;
-	}
-
-	public void cambiarCaballo(int caballo) {
-		cantCaballo = caballo;
 	}
 
 	public int caballos(int[] distancias) {
+		/*
+		 * distancias es la distancia en millas de una estación hasta la otra
+		 */
 
-		int distEstaciones = 0;
+		int distanciaEntreEstaciones = 0;
+		int cantCaballos = 1;
 
 		for (int i = 0; i < distancias.length; i++) {
 
-			distEstaciones = distancias[i] + distancias[i + 1];
-			if (distEstaciones <= 100) {
-				cambiarCaballo(getCaballo() + 1);
+			if (distancias[i] > 100) {
+				throw new Error("Ruta invalida");
+			}
+
+			if (distanciaEntreEstaciones + distancias[i] > 100) {
+				cantCaballos++;
+				distanciaEntreEstaciones = distancias[i];
+			} else {
+				distanciaEntreEstaciones += distancias[i];
 			}
 		}
 
-		return this.cantCaballo;
+		return cantCaballos;
+	}
+
+	public static void main(String[] args) {
+
+		Jinete jinete = new Jinete();
+
+		int sumaArreglo = jinete.caballos(new int[] { 43, 23, 40, 13 });
+		System.out.println(sumaArreglo);
 	}
 
 }
--- src/testLogPony/Caballo.java
+++ src/testLogPony/Caballo.java
@@ -11,8 +11,11 @@ class Caballo {
 	@Test
 	void test() {
 		Jinete jinete = new Jinete();
-
 		assertEquals(1, jinete.caballos(new int[] { 18, 15 }));
+		assertEquals(2, jinete.caballos(new int[] { 43, 23, 40, 13 }));
+		assertEquals(3, jinete.caballos(new int[] { 33, 8, 16, 47, 30, 30, 46 }));
+		assertEquals(3, jinete.caballos(new int[] { 51, 51, 51 }));
+		assertEquals(4, jinete.caballos(new int[] { 6, 24, 6, 8, 28, 8, 23, 47, 17, 29, 37, 18, 40, 49 }));
 	}
 
 }
